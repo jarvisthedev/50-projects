0ebe9eb72c7f4990497cb126bb0fc27ebd889203
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-04-17 13:50:46 +0300

    20th commit
    Completed the digital clock logic

--- 6. Digital clock/script.js
+++ 6. Digital clock/script.js
@@ -3,27 +3,34 @@ const containerDay = document.querySelector(".day");
 const containerTime = document.querySelector(".time");
 const containerDate = document.querySelector(".date");
 
-const newDate = new Date();
-console.log(newDate);
-console.log(
-  new Intl.DateTimeFormat("en-US", { month: "long" }).format(newDate)
-);
-console.log(
-  new Intl.DateTimeFormat("en-US", { weekday: "short" }).format(newDate)
-);
+function update_time() {
+  const newDate = new Date();
 
-const newDate_date = newDate.getDate();
-const newDate_month = newDate.getMonth();
-const newDate_day = newDate.getDay();
-const newDate_year = newDate.getFullYear();
-const newDate_hour = newDate.getHours();
-const newDate_min = newDate.getMinutes();
-const newDate_sec = newDate.getSeconds();
+  const newDate_month = new Intl.DateTimeFormat("en-US", {
+    month: "long",
+  }).format(newDate);
+  const newDate_day = new Intl.DateTimeFormat("en-US", {
+    weekday: "short",
+  }).format(newDate);
+  const newDate_date = newDate.getDate();
+  const newDate_year = newDate.getFullYear();
+  const newDate_hour = newDate.getHours();
+  const newDate_min = newDate.getMinutes();
+  const newDate_sec = newDate.getSeconds();
 
-console.log(newDate_date);
-console.log(newDate_month);
-console.log(newDate_day);
-console.log(newDate_year);
-console.log(newDate_hour);
-console.log(newDate_min);
-console.log(newDate_sec);
+  containerDay.textContent = newDate_day.toUpperCase();
+  containerDate.textContent = `${newDate_month.toUpperCase()}, ${newDate_date
+    .toString()
+    .padStart(2, 0)} ${newDate_year}`;
+
+  containerTime.innerHTML = `${newDate_hour
+    .toString()
+    .padStart(2, 0)} <span class="dot">:</span>${newDate_min
+    .toString()
+    .padStart(2, 0)} <span class="dot">:</span>${newDate_sec
+    .toString()
+    .padStart(2, 0)}`;
+}
+setInterval(() => {
+  update_time();
+}, 1000);
