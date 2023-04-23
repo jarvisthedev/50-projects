65cd9e4e0a0c4f41a3168831f9bca0068b978fc8
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-04-23 22:13:29 +0300

    33rd commit
    Added the date logic part. What is remaining is the API part of the weather app

--- 8. Weather web-app/script.js
+++ 8. Weather web-app/script.js
@@ -1,1 +1,26 @@
-const container = document.querySelector(".container");
+const inputText = document.querySelector(".input-text");
+const city = document.querySelector(".city");
+const dateIn = document.querySelector(".date");
+const temp = document.querySelector(".temp");
+const weather = document.querySelector(".weather");
+const hi_Low = document.querySelector(".hi-low");
+
+function dateUpdate() {
+  const newDate = new Date();
+
+  const date = newDate.getDate();
+  const year = newDate.getFullYear();
+  const month = new Intl.DateTimeFormat("en-US", {
+    month: "long",
+  }).format(newDate);
+  const day = new Intl.DateTimeFormat("en-US", {
+    weekday: "long",
+  }).format(newDate);
+
+  dateIn.textContent = `${day}, ${date} ${month} ${year}`;
+}
+
+dateUpdate();
+setInterval(() => {
+  dateUpdate();
+}, 1000);
