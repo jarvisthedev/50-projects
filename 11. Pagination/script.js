b60d6f8e75eebc7736b0a0b54a029f713aa8b445
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-05-07 23:58:31 +0300

    64th commit
    Added Scrolling logic to the " buttons " left and right.

--- 11. Pagination/script.js
+++ 11. Pagination/script.js
@@ -6,23 +6,47 @@ const pagesList = document.querySelector(".pages-list");
 const dotList = document.querySelector(".dots-list");
 const pageDetails = document.querySelectorAll(".page-detail");
 
+function currentPage_func(icon) {
+  let valueinput = pageDetails[0];
+  for (let i = 0; i < pageDetails.length; i++) {
+    if (pageDetails[i].classList.contains("current-page")) {
+      valueinput = pageDetails[i].textContent;
+      valueinput = Number(valueinput);
+    }
+  }
+  // for (let i = 0; i < pageDetails.length; i++) {
+  //   if (pageDetails[i].textContent === (valueinput + 1).toString()) {
+  //     pageDetails[i].classList.add("current-page");
+  //     pageDetails[i - 1].classList.remove("current-page");
+  //   }
+  // }
+
+  for (let i = 0; i < pageDetails.length; i++) {
+    if (
+      pageDetails[i].textContent ===
+      `${icon === icons[0] ? valueinput - 1 : valueinput + 1}`.toString()
+    ) {
+      pageDetails[i].classList.add("current-page");
+      pageDetails[`${icon === icons[0] ? i + 1 : i - 1}`].classList.remove(
+        "current-page"
+      );
+    }
+  }
+}
+
 container.addEventListener("click", function (e) {
+  e.preventDefault();
   const clicked = e.target;
 
   if (clicked.classList.contains("page-dots")) {
     dots.classList.toggle("hidden");
   }
 
-  if (clicked === icons[0]) {
-    console.log(11111);
-    for (let i = 0; i < pageDetails.length; i++) {
-      console.log(pageDetails[i]);
-    }
+  if (clicked.classList.contains("page-detail")) {
+    console.log("page clicked");
   }
 
-  if (clicked === icons[1]) {
-    console.log(33333);
-  }
+  if (clicked === icons[0] || clicked === icons[1]) currentPage_func(clicked);
 });
 
 console.log(pageDetails);
