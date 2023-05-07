d24757c1b1637b00133a29e2fec96f076e63cece
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-05-07 22:39:25 +0300

    63rd commit
    Added partial logic to the pagination

--- 11. Pagination/script.js
+++ 11. Pagination/script.js
@@ -1,5 +1,28 @@
 const container = document.querySelector(".container");
-// const container = document.querySelector(".container");
-// const container = document.querySelector(".container");
-// const container = document.querySelector(".container");
-// const container = document.querySelector(".container");
+const pageDots = document.querySelector(".page-dots");
+const dots = document.querySelector(".dots");
+const icons = document.querySelectorAll(".icon");
+const pagesList = document.querySelector(".pages-list");
+const dotList = document.querySelector(".dots-list");
+const pageDetails = document.querySelectorAll(".page-detail");
+
+container.addEventListener("click", function (e) {
+  const clicked = e.target;
+
+  if (clicked.classList.contains("page-dots")) {
+    dots.classList.toggle("hidden");
+  }
+
+  if (clicked === icons[0]) {
+    console.log(11111);
+    for (let i = 0; i < pageDetails.length; i++) {
+      console.log(pageDetails[i]);
+    }
+  }
+
+  if (clicked === icons[1]) {
+    console.log(33333);
+  }
+});
+
+console.log(pageDetails);
