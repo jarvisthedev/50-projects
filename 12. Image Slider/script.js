2f1dd78ff72a98c648eff2958b0940da9aa6e720
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-05-09 14:09:21 +0300

    69th commit
    Added styling and defined the images

--- 12. Image Slider/script.js
+++ 12. Image Slider/script.js
@@ -1,1 +1,12 @@
 const container = document.querySelector(".container");
+const imgList = document.querySelectorAll(".img-list");
+
+container.addEventListener("click", function (e) {
+  e.preventDefault();
+  const clicked = e.target;
+  if (!clicked.classList.contains("img-list")) return;
+  for (let i = 0; i < imgList.length; i++) {
+    imgList[i].classList.remove("current-img");
+  }
+  clicked.classList.add("current-img");
+});
