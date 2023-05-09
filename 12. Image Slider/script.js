e65b94c93b6d57ff9fe0ce604a89346bee3f9680
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-05-09 22:33:34 +0300

    70th commit
    Added flag logo icons and more styling of the page.

--- 12. Image Slider/script.js
+++ 12. Image Slider/script.js
@@ -1,12 +1,21 @@
 const container = document.querySelector(".container");
 const imgList = document.querySelectorAll(".img-list");
+const dots = document.querySelectorAll(".dot-list");
 
 container.addEventListener("click", function (e) {
   e.preventDefault();
   const clicked = e.target;
+  let curr = 0;
+
   if (!clicked.classList.contains("img-list")) return;
   for (let i = 0; i < imgList.length; i++) {
     imgList[i].classList.remove("current-img");
+    dots[i].classList.remove("current-dot");
   }
+
   clicked.classList.add("current-img");
+  for (let i = 0; i < imgList.length; i++) {
+    if (imgList[i].classList.contains("current-img")) curr = i;
+  }
+  dots[curr].classList.add("current-dot");
 });
