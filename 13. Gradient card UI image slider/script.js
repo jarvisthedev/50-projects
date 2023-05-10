bdd82bbdeb6f84c25b49c82f2b88b5f3d8855f22
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-05-10 23:57:34 +0300

    75th commit
    Added more styling and defining spacing and elements

--- 13. Gradient card UI image slider/script.js
+++ 13. Gradient card UI image slider/script.js
@@ -1,1 +1,26 @@
 const container = document.querySelector(".container");
+const cardImg = document.querySelectorAll(".card-img");
+const moreInfo = document.querySelectorAll(".more-info");
+// const container = document.querySelector(".container");
+
+container.addEventListener("click", function (e) {
+  console.log(11111);
+  let active = cardImg[0];
+  for (let i = 0; i < cardImg.length; i++) {
+    if (!cardImg[i].classList.contains("hidden")) {
+      console.log(i, cardImg[i]);
+      cardImg[i].classList.add("hidden");
+      moreInfo[i].classList.add("hidden");
+
+      active = i + 1;
+      console.log(active);
+    }
+    console.log(23323333);
+  }
+
+  moreInfo[active].classList.remove("hidden");
+  active - 1;
+  cardImg[active].classList.remove("hidden");
+});
+
+console.log(cardImg);
