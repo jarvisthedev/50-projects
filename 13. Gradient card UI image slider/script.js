7edbb41ae5ab41f867b61fa1c5b2066358567887
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-05-11 11:36:00 +0300

    76th commit
    Correccted the logic that caused an error and Made some few text errors

--- 13. Gradient card UI image slider/script.js
+++ 13. Gradient card UI image slider/script.js
@@ -1,26 +1,24 @@
 const container = document.querySelector(".container");
 const cardImg = document.querySelectorAll(".card-img");
 const moreInfo = document.querySelectorAll(".more-info");
-// const container = document.querySelector(".container");
+const dotDetail = document.querySelectorAll(".dot-detail");
 
 container.addEventListener("click", function (e) {
-  console.log(11111);
+  const clicked = e.target;
+  if (clicked.classList.contains("btn")) return;
   let active = cardImg[0];
   for (let i = 0; i < cardImg.length; i++) {
     if (!cardImg[i].classList.contains("hidden")) {
-      console.log(i, cardImg[i]);
       cardImg[i].classList.add("hidden");
       moreInfo[i].classList.add("hidden");
+      dotDetail[i].classList.remove("current-dot");
 
       active = i + 1;
-      console.log(active);
+      if (active === 3) active = 0;
     }
-    console.log(23323333);
   }
 
   moreInfo[active].classList.remove("hidden");
-  active - 1;
   cardImg[active].classList.remove("hidden");
+  dotDetail[active].classList.add("current-dot");
 });
-
-console.log(cardImg);
