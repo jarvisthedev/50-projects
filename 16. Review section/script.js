b65d5b9ff970afd933f7f68dd86c685998203fa5
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-05-16 23:59:48 +0300

    100th commit
    Added a logic to change hidden class

--- 16. Review section/script.js
+++ 16. Review section/script.js
@@ -1,1 +1,19 @@
 const sectionPricing = document.querySelector(".section--pricing");
+const review = document.querySelectorAll(".review");
+
+function addingRemovingHiddenClass() {
+  for (let i = 0; i < review.length; i++) {
+    let hiddenClass = 0;
+    if (!review[i].classList.includes("hidden")) {
+      review[i].classList.add("hidden");
+      hiddenClass += i + 2;
+      if (hiddenClass > review.length)
+        hiddenClass = review.length - hiddenClass;
+      review[hiddenClass].classList.add("hidden");
+    }
+  }
+}
+
+setInterval(() => {
+  addingRemovingHiddenClass();
+}, 1000 * 30);
