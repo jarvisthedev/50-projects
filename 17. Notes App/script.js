837d274fb9f7dcfa75f12664b78707c95f182e16
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-05-18 23:52:15 +0300

    108th commit
    Done with the logics

--- 17. Notes App/script.js
+++ 17. Notes App/script.js
@@ -1,8 +1,6 @@
 const sectionNotes = document.querySelector(".section--notes");
 
-const addNote = document.querySelector(".add");
 const inputText = document.querySelector(".input-text");
-const deleteNote = document.querySelectorAll(".delete");
 const notesSaved = document.querySelector(".notes-saved");
 
 sectionNotes.addEventListener("click", function (e) {
@@ -10,13 +8,17 @@ sectionNotes.addEventListener("click", function (e) {
   const clicked = e.target;
 
   if (clicked.classList.contains("add")) {
-    console.log(inputText.value);
-    const html = `<li class="notes">
+    if (inputText.value === "" || inputText.value === " ")
+      alert("Please add some text");
+    // console.log(inputText.value);
+    else {
+      const html = `<li class="notes">
       <div class="note">${inputText.value}</div>
       <div class="delete">x</div>
       </li>`;
-    inputText.value = "";
-    notesSaved.insertAdjacentHTML("afterbegin", html);
+      inputText.value = "";
+      notesSaved.insertAdjacentHTML("afterbegin", html);
+    }
   }
 
   if (clicked.classList.contains("delete")) {
