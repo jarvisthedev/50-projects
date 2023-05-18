95e1a0942fe8acf17989f742d9d2d8febbf74c6a
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-05-18 23:37:20 +0300

    107th commit
    Added more logic on saving the note and deleting the note

--- 17. Notes App/script.js
+++ 17. Notes App/script.js
@@ -1,1 +1,25 @@
-// const sectionPricing = document.querySelector(".section--pricing");
+const sectionNotes = document.querySelector(".section--notes");
+
+const addNote = document.querySelector(".add");
+const inputText = document.querySelector(".input-text");
+const deleteNote = document.querySelectorAll(".delete");
+const notesSaved = document.querySelector(".notes-saved");
+
+sectionNotes.addEventListener("click", function (e) {
+  e.preventDefault();
+  const clicked = e.target;
+
+  if (clicked.classList.contains("add")) {
+    console.log(inputText.value);
+    const html = `<li class="notes">
+      <div class="note">${inputText.value}</div>
+      <div class="delete">x</div>
+      </li>`;
+    inputText.value = "";
+    notesSaved.insertAdjacentHTML("afterbegin", html);
+  }
+
+  if (clicked.classList.contains("delete")) {
+    clicked.parentNode.remove();
+  }
+});
