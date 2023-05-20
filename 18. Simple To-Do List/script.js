7a412a75ff587bcb3dd456861271f0527bb2dee4
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-05-20 23:53:39 +0300

    115th commit
    Added styling and "deletion" logic

--- 18. Simple To-Do List/script.js
+++ 18. Simple To-Do List/script.js
@@ -1,1 +1,8 @@
-const sectionNotes = document.querySelector(".section--notes");
+const sectionToDoList = document.querySelector(".section--to-do-list ");
+
+sectionToDoList.addEventListener("click", function (e) {
+  const clicked = e.target;
+  if (clicked.classList.contains("delete")) {
+    clicked.parentNode.remove();
+  }
+});
