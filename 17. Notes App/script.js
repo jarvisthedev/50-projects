6f69368e982e25b7194afa0e7d4211acd10d92b4
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-05-20 15:17:12 +0300

    112th commit
    COLOR - Bug fixing

--- 17. Notes App/script.js
+++ 17. Notes App/script.js
@@ -1,38 +1,31 @@
 const sectionNotes = document.querySelector(".section--notes");
-
+const noteSaved = document.querySelector(".notes-saved");
 const inputText = document.querySelector(".input-text");
-const notesSaved = document.querySelector(".notes-saved");
-const colorChange = document.querySelector(".color");
+const addNote = document.querySelector(".add");
 
-function clickEvent(e) {
-  e.preventDefault();
-  const clicked = e.target;
-
-  if (clicked.classList.contains("add")) {
-    if (inputText.value === "" || inputText.value === " ")
-      alert("Please add some text");
-    else {
-      const html = `<li class="notes">
+addNote.addEventListener("click", function (e) {
+  if (inputText.value === "" || inputText.value === " ")
+    alert("Please add some text");
+  else {
+    const html = `<li class="notes">
       <div class="note">${inputText.value}</div>
       <div class="delete">x</div>
       </li>`;
-      inputText.value = "";
-      notesSaved.insertAdjacentHTML("afterbegin", html);
-    }
+    inputText.value = "";
+    noteSaved.insertAdjacentHTML("afterbegin", html);
   }
+});
 
+noteSaved.addEventListener("click", function (e) {
+  const clicked = e.target;
   if (clicked.classList.contains("delete")) {
     clicked.parentNode.remove();
   }
-}
+});
 
-sectionNotes.addEventListener("click", clickEvent);
-
-sectionNotes.removeEventListener("click", clickEvent);
 sectionNotes.addEventListener("change", function (e) {
   if (e.target.classList.contains("color")) {
     const selectedColor = e.target.value;
     inputText.style.color = selectedColor;
   }
-  sectionNotes.addEventListener("click", clickEvent);
 });
