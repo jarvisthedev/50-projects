de5eb2fe9a8a3f0378170d036a61c664f962b311
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-05-25 23:27:37 +0300

    135th commit
    Added file logic

--- 23. Hero section./script.js
+++ 23. Hero section./script.js
@@ -1,1 +1,13 @@
-const sectionHero = document.querySelector(".section--hero");
+const header = document.querySelector(".header");
+const navDetail = document.querySelectorAll(".nav-detail");
+const moreText = document.querySelectorAll(".more-text");
+
+header.addEventListener("click", function (e) {
+  const clicked = e.target;
+  if (clicked) {
+    console.log(clicked);
+    if (clicked.classList.contains("nav-detail")) {
+      console.log(clicked);
+    }
+  }
+});
