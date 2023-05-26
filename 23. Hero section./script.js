e8e76a12afb074be9e64fc2e95a37dc3923d1578
dotjarvis <johnsonmakings@gmail.com>
2023-05-26 21:49:02 +0300

    137th commit
    Corrected the logic

--- 23. Hero section./script.js
+++ 23. Hero section./script.js
@@ -1,8 +1,7 @@
 const header = document.querySelector(".header");
+const moreText = document.querySelector(".more-text");
 const navDetail = document.querySelectorAll(".nav-detail");
-const moreText = document.querySelectorAll(".more-text");
 
-// Define the dynamic image URL
 let dynamicImageUrl = "./imgs/see-life.jpg";
 let gradient =
   "linear-gradient(to right bottom, rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.35))";
@@ -10,28 +9,25 @@ let gradient =
 header.addEventListener("click", function (e) {
   e.preventDefault();
   const clicked = e.target;
-  if (clicked) {
-    console.log(clicked);
-    if (clicked.classList.contains("nav-detail")) {
-      //     background-image: linear-gradient(
-      //   to right bottom,
-      //   rgba(0, 0, 0, 0.35),
-      //   rgba(0, 0, 0, 0.35)
-      // ),
-      // url("./imgs/see-life.jpg");
-
-      if (clicked.classList.contains("sea")) {
-        dynamicImageUrl = "./imgs/see-life.jpg";
-      } else if (clicked.classList.contains("forest")) {
-        dynamicImageUrl = "./imgs/forest-dark.jpg";
-      } else if (clicked.classList.contains("skiing")) {
-        dynamicImageUrl = "./imgs/skiing.jpg";
-      } else if (clicked.classList.contains("biking")) {
-        dynamicImageUrl = "./imgs/biking.jpg";
-      } else if (clicked.classList.contains("golfing")) {
-        dynamicImageUrl = "./imgs/golf1.jpg";
-      }
+  if (clicked.classList.contains("nav-detail")) {
+    if (clicked.classList.contains("sea")) {
+      dynamicImageUrl = "./imgs/see-life.jpg";
+      moreText.textContent = "Sea Life";
+    } else if (clicked.classList.contains("forest")) {
+      dynamicImageUrl = "./imgs/forest-dark.jpg";
+      moreText.textContent = "Forest Life";
+    } else if (clicked.classList.contains("skiing")) {
+      dynamicImageUrl = "./imgs/skiing1.jpg";
+      moreText.textContent = "Skiing";
+    } else if (clicked.classList.contains("biking")) {
+      dynamicImageUrl = "./imgs/biking.jpg";
+      moreText.textContent = "Biking";
+    } else if (clicked.classList.contains("golfing")) {
+      dynamicImageUrl = "./imgs/golf1.jpg";
+      moreText.textContent = "Golfing";
     }
-    header.style.backgroundImage = `${gradient}, url("${dynamicImageUrl}")`;
   }
+  header.style.backgroundImage = `${gradient}, url("${dynamicImageUrl}")`;
 });
+
+console.log(moreText.textContent);
