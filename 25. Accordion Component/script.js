fd783ba7a038d0a38a20ebf3a74eac146b7bed54
dotjarvis <johnsonmakings@gmail.com>
2023-05-27 11:06:53 +0300

    142 commit
    Added the project logic

--- 25. Accordion Component/script.js
+++ 25. Accordion Component/script.js
@@ -1,1 +1,15 @@
 const sectionAccordion = document.querySelector(".section--accordion");
+// component
+// arrow-down
+//
+// response
+
+sectionAccordion.addEventListener("click", function (e) {
+  const clicked = e.target;
+  if (!clicked.classList.contains("ph-thin")) return;
+  const parentComponent = clicked.closest(".component");
+  const response = parentComponent.querySelector(".response");
+
+  console.log(parentComponent);
+  console.log(response);
+});
