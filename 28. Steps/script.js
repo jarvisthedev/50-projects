5b5ac527ec14bd075b8766b5b063082d83ab2cfe
dotjarvis <johnsonmakings@gmail.com>
2023-06-02 23:45:51 +0300

    165th commit
    Added new images

--- 28. Steps/script.js
+++ 28. Steps/script.js
@@ -1,1 +1,8 @@
 const sectionSteps = document.querySelector(".section--steps");
+
+const button = document.querySelector(button);
+const article = document.querySelector(".article");
+const icons = document.querySelector(".icon");
+const number = document.querySelector(".number");
+const articleContent = document.querySelector(".article-content");
+const articleTitle = document.querySelector(".article-title");
