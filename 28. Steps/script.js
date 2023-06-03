7c06982eb56d43e6a6ca43b21f71e4bf3fe1460e
dotjarvis <johnsonmakings@gmail.com>
2023-06-03 23:51:35 +0300

    168th commit
    Added logic to the project

--- 28. Steps/script.js
+++ 28. Steps/script.js
@@ -1,8 +1,35 @@
-const sectionSteps = document.querySelector(".section--steps");
+const sectionSteps = document.querySelector(".container");
 
-const button = document.querySelector(button);
-const article = document.querySelector(".article");
+const button = document.querySelector("button");
+const article = document.querySelectorAll(".article");
 const icons = document.querySelector(".icon");
-const number = document.querySelector(".number");
+const number = document.querySelectorAll(".number");
 const articleContent = document.querySelector(".article-content");
-const articleTitle = document.querySelector(".article-title");
+const articleTitle = document.querySelectorAll(".article-title");
+
+// clicked-active
+// article--hover
+
+sectionSteps.addEventListener("click", function (e) {
+  const parent = e.target.closest(".article");
+  for (let i = 0; i < article.length; i++) {
+    article[i].classList.remove("clicked-active");
+    article[i].classList.remove("centering-all--items");
+    number[i].classList.add("hidden");
+    articleTitle[i].classList.remove("hidden");
+    articleTitle[i].classList.add("vertical-title");
+  }
+
+  parent.classList.add("clicked-active");
+  parent.querySelector(".article-title").classList.add("hidden");
+});
+
+// sectionSteps.addEventListener("mouseover", function (e) {
+//   const parent = e.target.closest(".article");
+//   for (let i = 0; i < article.length; i++) {
+//     article[i].classList.remove("article--hover");
+//   }
+
+//   parent.classList.add("article--hover");
+//   parent.querySelector(".number").classList.remove("hidden");
+// });
