9a7ea2451fadfd2b44ae3d4d13c01d0225c33568
dotjarvis <johnsonmakings@gmail.com>
2023-06-06 23:53:52 +0300

    179th commit
    Added hover effect

--- 30. Navigation/script.js
+++ 30. Navigation/script.js
@@ -1,2 +1,15 @@
-const sectionNavigation = document.querySelector(".section--navigation");
+const sectionHead = document.querySelector(".section--head");
 const container = document.querySelector(".container");
+
+sectionHead.addEventListener("mouseover", function (e) {
+  e.preventDefault();
+  const parentHover = e.target.closest(".parent-header-hover");
+  //   child-header-hover
+  const childHover = parentHover.querySelector(".child-header-hover");
+  // if (childHover.classList.remove("hidden")) {
+  // childHover.classList.remove("hidden");
+  // }
+  console.log(1122);
+  console.log(parentHover);
+  console.log(childHover);
+});
