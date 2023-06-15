33ad8b2ea5c9b68f6842853c8c18f198b5e851f2
dotjarvis <johnsonmakings@gmail.com>
2023-06-15 21:24:22 +0300

    222 commit
    Restructuring and making it responsive

--- 11. Pagination/script.js
+++ 11. Pagination/script.js
@@ -1,14 +1,14 @@
-const container = document.querySelector(".container");
-const dots = document.querySelector(".dots");
-const icons = document.querySelectorAll(".icon");
-const pagesList = document.querySelector(".pages-list");
-const dotList = document.querySelector(".dots-list");
-const pageDetails = document.querySelectorAll(".page-detail");
+const container = document.querySelector('.container');
+const dots = document.querySelector('.dots');
+const icons = document.querySelectorAll('.ph');
+const pagesList = document.querySelector('.pages-list');
+const dotList = document.querySelector('.dots-list');
+const pageDetails = document.querySelectorAll('.page-detail');
 
 function currentPage_func(icon) {
   let valueinput = pageDetails[0];
   for (let i = 0; i < pageDetails.length; i++) {
-    if (pageDetails[i].classList.contains("current-page")) {
+    if (pageDetails[i].classList.contains('current-page')) {
       valueinput = pageDetails[i].textContent;
       valueinput = Number(valueinput);
     }
@@ -19,33 +19,33 @@ function currentPage_func(icon) {
       pageDetails[i].textContent ===
       `${icon === icons[0] ? valueinput - 1 : valueinput + 1}`.toString()
     ) {
-      pageDetails[i].classList.add("current-page");
+      pageDetails[i].classList.add('current-page');
       pageDetails[`${icon === icons[0] ? i + 1 : i - 1}`].classList.remove(
-        "current-page"
+        'current-page'
       );
     }
   }
 }
 
-container.addEventListener("click", function (e) {
+container.addEventListener('click', function (e) {
   e.preventDefault();
   const clicked = e.target;
 
   if (clicked === icons[0] || clicked === icons[1]) currentPage_func(clicked);
-  if (clicked.classList.contains("page-dots")) dots.classList.toggle("hidden");
+  if (clicked.classList.contains('page-dots')) dots.classList.toggle('hidden');
 
-  if (clicked.classList.contains("page-detail"))
+  if (clicked.classList.contains('page-detail'))
     for (let i = 0; i < pageDetails.length; i++) {
-      pageDetails[i].classList.remove("current-page");
-      clicked.classList.add("current-page");
+      pageDetails[i].classList.remove('current-page');
+      clicked.classList.add('current-page');
     }
 
   for (let i = 0; i < pageDetails.length; i++) {
     if (
       7 < Number(pageDetails[i].textContent) &&
       Number(pageDetails[i].textContent) < 17 &&
-      pageDetails[i].classList.contains("current-page")
+      pageDetails[i].classList.contains('current-page')
     )
-      dots.classList.remove("hidden");
+      dots.classList.remove('hidden');
   }
 });
