8109d4f5a3471debc3a01430703f77ad773ac60c
dotjarvis <johnsonmakings@gmail.com>
2023-06-17 23:59:47 +0300

    231 commit
    added reposnvoness

--- 14. Frequently Asked Question/script.js
+++ 14. Frequently Asked Question/script.js
@@ -1,35 +1,35 @@
-const sectionFaq = document.querySelector(".section--faq");
-const quizText = document.querySelectorAll(".quiz-text");
-const downArrow = document.querySelectorAll(".ph-caret-down");
-const upArrow = document.querySelectorAll(".ph-caret-up");
+const sectionFaq = document.querySelector('.section--faq');
+const quizText = document.querySelectorAll('.quiz-text');
+const downArrow = document.querySelectorAll('.ph-caret-down');
+const upArrow = document.querySelectorAll('.ph-caret-up');
 
-sectionFaq.addEventListener("click", function (e) {
+sectionFaq.addEventListener('click', function (e) {
   const clicked = e.target;
-  const up__arrow = clicked.classList.contains("ph-caret-up");
-  const down__arrow = clicked.classList.contains("ph-caret-down");
+  const up__arrow = clicked.classList.contains('ph-caret-up');
+  const down__arrow = clicked.classList.contains('ph-caret-down');
 
   if (up__arrow || down__arrow) {
     const up_arrow = clicked.nextElementSibling;
-    const quizTextElement = clicked.closest(".quiz-top").nextElementSibling;
+    const quizTextElement = clicked.closest('.quiz-icon').nextElementSibling;
 
     if (down__arrow) {
       for (let i = 0; i < quizText.length; i++) {
-        quizText[i].classList.add("hidden");
-        downArrow[i].classList.remove("hidden");
-        upArrow[i].classList.add("hidden");
+        quizText[i].classList.add('hidden');
+        downArrow[i].classList.remove('hidden');
+        upArrow[i].classList.add('hidden');
       }
 
-      clicked.classList.add("hidden");
-      up_arrow.classList.remove("hidden");
-      quizTextElement.classList.remove("hidden");
+      clicked.classList.add('hidden');
+      up_arrow.classList.remove('hidden');
+      quizTextElement.classList.remove('hidden');
     }
 
     if (up__arrow) {
       const down_arrow = clicked.previousElementSibling;
-      down_arrow.classList.remove("hidden");
-      quizTextElement.classList.add("hidden");
 
-      clicked.classList.add("hidden");
+      clicked.classList.add('hidden');
+      down_arrow.classList.remove('hidden');
+      quizTextElement.classList.add('hidden');
     }
   }
 });
