66d49adabb6891f5fd65eea2db8baddbec50493a
dotjarvis <johnsonmakings@gmail.com>
2023-06-19 19:28:45 +0300

    241 commit
    Restructuring the preoject and making it responsive

--- 18. Simple To-Do List/script.js
+++ 18. Simple To-Do List/script.js
@@ -4,34 +4,34 @@ const userInput = document.querySelector('.user-input');
 
 sectionToDoList.addEventListener('click', function (e) {
   const clicked = e.target;
-  if (clicked.classList.contains('delete')) clicked.parentNode.remove();
+  if (clicked.classList.contains('ph-x')) clicked.parentNode.remove();
 
-  if (clicked.parentNode.classList.contains('adding-todo')) {
+  if (clicked.parentNode.classList.contains('add-todo')) {
     if (userInput.value === '' || userInput.value === ' ')
       alert('Please add some text');
     else if (userInput.value.length > 40)
       alert('Maximum of 40 characters needed');
     else {
       const html = `
-        <li class="do-detail">
+        <li class="list-detail">
           <hr class="hr" />
-          <ion-icon class="icon checked hidden" name="checkbox-outline"></ion-icon>
-          <ion-icon class="icon unchecked" name="square-outline"></ion-icon>
+          <i class="ph hidden ph-check-square"></i>
+          <i class="ph ph-square"></i>
           <p class="todo-text">${userInput.value}</p>
-          <ion-icon class="icon delete" name="close-outline"></ion-icon>
+          <i class="ph ph-x"></i>
         </li>`;
       userInput.value = '';
       toDoList.insertAdjacentHTML('afterbegin', html);
     }
   }
 
-  if (clicked.classList.contains('unchecked')) {
+  if (clicked.classList.contains('ph-square')) {
     clicked.classList.add('hidden');
     clicked.previousElementSibling.classList.remove('hidden');
     clicked.nextElementSibling.style.textDecoration = 'line-through';
   }
 
-  if (clicked.classList.contains('checked')) {
+  if (clicked.classList.contains('ph-check-square')) {
     clicked.classList.add('hidden');
     clicked.nextElementSibling.classList.remove('hidden');
     clicked.nextElementSibling.nextElementSibling.style.textDecoration = 'none';
