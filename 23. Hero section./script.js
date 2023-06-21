0ca47481690f41258190e2c3290232c7be7ce0b4
dotjarvis <johnsonmakings@gmail.com>
2023-06-21 09:10:47 +0300

    246 commit
    Resctucturing project

--- 23. Hero section./script.js
+++ 23. Hero section./script.js
@@ -1,30 +1,30 @@
-const header = document.querySelector(".header");
-const moreText = document.querySelector(".more-text");
-const navDetail = document.querySelectorAll(".nav-detail");
+const header = document.querySelector('.header');
+const primaryText = document.querySelector('.primary-text');
+const navDetail = document.querySelectorAll('.nav');
 
-let dynamicImageUrl = "./imgs/see-life.jpg";
+let dynamicImageUrl = './imgs/see-life.jpg';
 let gradient =
-  "linear-gradient(to right bottom, rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.35))";
+  'linear-gradient(to right bottom, rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.35))';
 
-header.addEventListener("click", function (e) {
+header.addEventListener('click', function (e) {
   e.preventDefault();
   const clicked = e.target;
-  if (clicked.classList.contains("nav-detail")) {
-    if (clicked.classList.contains("sea")) {
-      dynamicImageUrl = "./imgs/see-life.jpg";
-      moreText.textContent = "Sea Life";
-    } else if (clicked.classList.contains("forest")) {
-      dynamicImageUrl = "./imgs/forest-dark.jpg";
-      moreText.textContent = "Forest Life";
-    } else if (clicked.classList.contains("skiing")) {
-      dynamicImageUrl = "./imgs/skiing1.jpg";
-      moreText.textContent = "Skiing";
-    } else if (clicked.classList.contains("biking")) {
-      dynamicImageUrl = "./imgs/biking.jpg";
-      moreText.textContent = "Biking";
-    } else if (clicked.classList.contains("golfing")) {
-      dynamicImageUrl = "./imgs/golf1.jpg";
-      moreText.textContent = "Golfing";
+  if (clicked.classList.contains('nav')) {
+    if (clicked.classList.contains('sea')) {
+      dynamicImageUrl = './imgs/see-life.jpg';
+      primaryText.textContent = 'Sea Life';
+    } else if (clicked.classList.contains('forest')) {
+      dynamicImageUrl = './imgs/forest-dark.jpg';
+      primaryText.textContent = 'Forest Life';
+    } else if (clicked.classList.contains('skiing')) {
+      dynamicImageUrl = './imgs/skiing1.jpg';
+      primaryText.textContent = 'Skiing';
+    } else if (clicked.classList.contains('biking')) {
+      dynamicImageUrl = './imgs/biking.jpg';
+      primaryText.textContent = 'Biking';
+    } else if (clicked.classList.contains('golfing')) {
+      dynamicImageUrl = './imgs/golf1.jpg';
+      primaryText.textContent = 'Golfing';
     }
   }
   header.style.backgroundImage = `${gradient}, url("${dynamicImageUrl}")`;
