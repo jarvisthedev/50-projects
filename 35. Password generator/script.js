8a8f8a42096e18aee3786f96153bdb9ea6208f6e
dotjarvis <johnsonmakings@gmail.com>
2023-06-26 23:59:31 +0300

    268th commit
    Added responsiveness

--- 35. Password generator/script.js
+++ 35. Password generator/script.js
@@ -1,1 +1,13 @@
 `use strict`;
+
+const sectionPassword = document.querySelector('.section--password');
+// section--password
+
+sectionPassword.addEventListener('click', function (e) {
+  console.log(e.target.checked);
+  // Check the checkbox programmatically
+  //   checkbox.checked = true;
+
+  // Uncheck the checkbox programmatically
+  //   checkbox.checked = false;
+});
