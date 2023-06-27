1f4925bf40190dd2b0791f6de5085366259369c2
dotjarvis <johnsonmakings@gmail.com>
2023-06-27 21:22:41 +0300

    270th commit
    Added Partial logic

--- 35. Password generator/script.js
+++ 35. Password generator/script.js
@@ -2,25 +2,33 @@
 
 const sectionPassword = document.querySelector('.section--password');
 
+const pswrdLen = document.querySelector('.pswrd_len');
 const uppercaseInc = document.querySelector('.uppercase_inc');
 const lowercaseInc = document.querySelector('.lowercase_inc');
 const numberInc = document.querySelector('.number_inc');
 const symbolsInc = document.querySelector('.symbols_inc');
-// section--password
+
+function uniqueChar() {
+  const uniqueChars = `"~!@#$%^&*()-_+={}[]|\:;"'<>,.?/`;
+  return uniqueChars[Math.trunc(Math.random() * uniqueChars.length)];
+}
+
+function uniqAlphabet() {
+  const alphabets = 'abcdefghijklmnopqrstuvwxyz';
+  return alphabets[Math.trunc(Math.random() * alphabets.length)];
+}
 
 sectionPassword.addEventListener('click', function (e) {
-  //   console.log(e.target.checked);
-  // Check the checkbox programmatically
-  //   checkbox.checked = true;
-
-  // Uncheck the checkbox programmatically
-  //   checkbox.checked = false;
-
-  console.log(uppercaseInc.checked);
-  console.log(lowercaseInc.checked);
-  console.log(numberInc.checked);
-  console.log(symbolsInc.checked);
-
-  if (e.target.closest('.generate')) console.log(1111111);
-  if (e.target.closest('.copy')) console.log(2222222);
+  const passLength = pswrdLen.value;
+  if (passLength < 8 || passLength > 16) return;
+  //   console.log(passLength);
+  let finalPassword;
+  for (let i = 1; i < passLength; i++) {
+    console.log(i);
+    const char = uniqAlphabet();
+    console.log(char);
+    finalPassword += char;
+  }
+  // if (e.target.closest('.copy')) console.log(2222222);
+  if (e.target.closest('.generate')) console.log(finalPassword);
 });
