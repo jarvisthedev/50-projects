c32d49fd378ec42a4138bf88899b7daeda4f7504
dotjarvis <johnsonmakings@gmail.com>
2023-06-30 23:07:00 +0300

    285th commit
    started a new project

---
+++ 37. Checkout Section (copy)/script.js
@@ -0,0 +1,18 @@
+`use strict`;
+
+const sectioncheckout = document.querySelector('.section--checkout');
+const dots = document.querySelectorAll('.dot');
+const images = document.querySelectorAll('.watch-img');
+
+sectioncheckout.addEventListener('click', function (e) {
+  const clicked = e.target;
+  if (clicked.classList.contains('dot')) {
+    Array.from(dots).map(el => el.classList.remove('current-img'));
+    Array.from(images).map(el => el.classList.add('hidden'));
+    clicked.classList.add('current-img');
+
+    for (let i = 0; i < dots.length; i++)
+      if (dots[i].classList.contains('current-img'))
+        images[i].classList.remove('hidden');
+  }
+});
