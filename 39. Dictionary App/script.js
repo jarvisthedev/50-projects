a5b124f8b446347c247054bb55ae8765ff0cd0be
dotjarvis <johnsonmakings@gmail.com>
2023-07-02 12:23:01 +0300

    293th commit
    Restructure to make the project work

--- 39. Dictionary App/script.js
+++ 39. Dictionary App/script.js
@@ -1,56 +1,54 @@
 'use strict';
 
-const sectionDict = document.querySelector('.section--dictionary');
 const url = `https://api.dictionaryapi.dev/api/v2/entries/en/`;
 
+const sectionDict = document.querySelector('.section--dictionary');
 const wordInfor = document.querySelector('.word-infor');
 const wordSearch = document.querySelector('.word-input');
 const btn = document.querySelector('.btn');
-const sound = document.querySelector('.ph-speaker-high');
 
 sectionDict.addEventListener('click', function (e) {
   e.preventDefault();
   let wordInput = wordSearch.value;
   if (e.target === btn) {
     fetch(`${url} ${wordInput}`)
-      .then(response => {
-        if (response.status === 404) {
-          wordInfor.innerHTML = '';
-          return wordInfor.insertAdjacentHTML(
-            'beforeend',
-            `<p>Word not found try another word</p>`
-          );
-        } else return response.json();
-      })
+      .then(response => response.json())
       .then(data => {
         const html = `
-              <div class="word-speaker">
-                <h2 class="primary-text">${data[0].word}</h2>
-                <p><i class="ph ph-speaker-high"></i></p>
-              </div>
-              <p class="span">
-                <span class="language">${
-                  data[0].meanings[0].partOfSpeech
-                } </span>/<span class="propounce"
-                  >${data[0]?.phonetic ?? ''}</span
-                >/
-              </p>
-              <p class="word-meaning">
-                ${data[0].meanings[0].definitions[0].definition}
-              </p>
-              <p class="word-example">
-                ${data[0].meanings[0].definitions[0]?.example ?? ''}
-              </p>`;
+            <div class="word-speaker">
+              <h2 class="primary-text">${data[0].word}</h2>
+              <p><i class="ph ph-speaker-high"></i></p>
+            </div>
+            <p class="span">
+              <span class="language">${
+                data[0].meanings[0].partOfSpeech
+              } </span>/<span class="propounce"
+                >${data[0]?.phonetic ?? ''}</span
+              >/
+            </p>
+            <p class="word-meaning">
+              ${data[0].meanings[0].definitions[0].definition}
+            </p>
+            <p class="word-example">
+              ${data[0].meanings[0].definitions[0]?.example ?? ''}
+            </p>
+        `;
+
         wordInfor.innerHTML = '';
         wordInfor.insertAdjacentHTML('beforeend', html);
+      })
+      .catch(() => {
+        wordInfor.insertAdjacentHTML(
+          'beforeend',
+          `<p>Word not found try another word</p>`
+        );
       });
   }
-});
 
-// if (response.status === 404) {
-//   console.log('Resource not found!');
-//   // Handle the error - display an error message, redirect, or retry the request
-// } else {
-//   // Handle the successful response
-//   return response.json(); // Assuming the response is in JSON format
-// }
+  if (e.target.classList.contains('ph-speaker-high'))
+    if ('speechSynthesis' in window) {
+      const utterance = new SpeechSynthesisUtterance();
+      utterance.text = `${wordInput}`;
+      speechSynthesis.speak(utterance);
+    }
+});
