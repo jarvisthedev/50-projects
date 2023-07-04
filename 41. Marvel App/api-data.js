0bc602654106d3171012adf7356d90e35d6fc035
dotjarvis <johnsonmakings@gmail.com>
2023-07-04 11:09:45 +0300

    299th commit
    Added logi

---
+++ 41. Marvel App/api-data.js
@@ -0,0 +1,4 @@
+const ts = '1688457876332';
+const publicKey = '50460563d602749b4ee43a6c2bad2e23';
+const hashVal = '0a435fe9a9f52b5077a9d3fd408229e4';
+const privateKey = '7fc28400be193cf923539a6cd50a6bdace5ffd38';
--- 41. Marvel App/script.js
+++ 41. Marvel App/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const sectionMarvel = document.querySelector('.section--marvel');
+const input = document.querySelector('.input-text');
+const searchBtn = document.querySelector('.search-btn');
+const marvelContainerInfo = document.querySelector('.marvel-info');
+
+const date = new Date();
+const [timestamp, apiKey, hashValue] = [ts, publicKey, hashVal];
+
+const getResult = async () => {
+  //   if (input.value.trim().length < 1) alert('Input cannot be blank');
+  console.log(`${input.value} value`);
+  marvelContainerInfo.innerHTML = '';
+  const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}&name=${input.value}`;
+
+  const response = await fetch(url);
+  const jsonData = await response.json();
+  jsonData.data['results'].forEach(el => {
+    marvelContainerInfo.innerHTML += `
+      <div class='card-container'>
+          <div class='container-character-image'>
+              <img src='${
+                el.thumbnail['path'] + '.' + el.thumbnail['extension']
+              }'>
+          </div>
+          <div class='character-name'>${el.description}</div>
+      </div>`;
+  });
+};
+
+searchBtn.addEventListener('click', getResult);
+window.onload = () => {
+  getResult();
+};
