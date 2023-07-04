2889e4f92766f28c07455a3f453c25285c3e464d
dotjarvis <johnsonmakings@gmail.com>
2023-07-04 13:11:48 +0300

    300th commit
    COrrected logic error

--- 41. Marvel App/script.js
+++ 41. Marvel App/script.js
@@ -4,32 +4,69 @@ const sectionMarvel = document.querySelector('.section--marvel');
 const input = document.querySelector('.input-text');
 const searchBtn = document.querySelector('.search-btn');
 const marvelContainerInfo = document.querySelector('.marvel-info');
+const listContainer = document.querySelector('.list-container');
 
 const date = new Date();
 const [timestamp, apiKey, hashValue] = [ts, publicKey, hashVal];
 
-const getResult = async () => {
-  //   if (input.value.trim().length < 1) alert('Input cannot be blank');
-  console.log(`${input.value} value`);
+const getResult = async e => {
+  e.preventDefault();
+  if (input.value.trim().length < 1) alert('Input cannot be blank');
+
   marvelContainerInfo.innerHTML = '';
   const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}&name=${input.value}`;
 
   const response = await fetch(url);
   const jsonData = await response.json();
   jsonData.data['results'].forEach(el => {
-    marvelContainerInfo.innerHTML += `
-      <div class='card-container'>
-          <div class='container-character-image'>
-              <img src='${
+    marvelContainerInfo.innerHTML = `
+       <div class="marvel-info">
+          <div class="search-output">
+            <div
+              role="img"
+              aria-label="Marvel image found"
+              class="marvel-img-holder"
+            >
+              <img class="marvel-img" src='${
                 el.thumbnail['path'] + '.' + el.thumbnail['extension']
               }'>
+            </div>
+            <h2 class="marvel-name">${el.name}</h2>
           </div>
-          <div class='character-name'>${el.description}</div>
-      </div>`;
+
+          <div class="detailed-marvel">
+            <p class="detailed-text">${el.description}</p>
+          </div>
+        </div>
+
+      `;
   });
 };
 
 searchBtn.addEventListener('click', getResult);
-window.onload = () => {
-  getResult();
-};
+
+const removeELements = () => (listContainer.innerHTML = '');
+
+function displayWords(value) {
+  input.value = value;
+  removeELements();
+}
+
+input.addEventListener('keyup', async () => {
+  removeELements();
+  if (input.value.length < 4) return false;
+
+  const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}&nameStartsWith=${input.value}`;
+
+  const response = await fetch(url);
+  const jsonData = await response.json();
+
+  jsonData.data['results'].forEach(result => {
+    let name = result.name;
+    let word = `<b>${name.substr(0, input.value.length)}</b>${name.substr(
+      input.value.length
+    )}`;
+    let html = `<div class="autocomplete-items" onclick="displayWords('${name}')"><p class="item">${word}</p></div>`;
+    listContainer.insertAdjacentHTML('beforeend', html);
+  });
+});
