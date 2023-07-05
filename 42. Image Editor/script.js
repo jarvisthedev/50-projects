cc87f4b80df0dc0543073a81caecf8d98f1aea70
dotjarvis <johnsonmakings@gmail.com>
2023-07-06 00:21:19 +0300

    305th commit
    Added half logic ... Made the logic in a function handler(ROtateAndFUnctionHandler()

--- 42. Image Editor/script.js
+++ 42. Image Editor/script.js
@@ -3,31 +3,38 @@
 const sectionEditor = document.querySelector('.section--editor');
 const img = document.querySelector('img');
 
-const rotate_flip = function (ph) {};
+const rotate_left = document.querySelector(`.rotate--left`);
+const rotate_right = document.querySelector(`.rotate--right`);
+const flip_left = document.querySelector(`.flip--left`);
+const flip_btm = document.querySelector(`.flip--bottom`);
 
-sectionEditor.addEventListener('click', function (e) {
-  let rotateLeft = 0;
-  let rotateRight = 0;
-  let flipRigth = -1;
-  let flipBtm = -1;
+let rotateLeft = 0;
+let rotateRight = 0;
+let flipRigth = -1;
+let flipBtm = -1;
 
-  if (e.target.classList.contains('rotate--left')) {
+const rotateAndFlipFunctionHandler = function (event) {
+  if (event === rotate_left) {
     rotateLeft = rotateLeft + -90;
     img.style.transform = `rotate(${rotateLeft}deg)`;
   }
 
-  if (e.target.classList.contains('rotate--right')) {
+  if (event === rotate_right) {
     rotateRight = rotateRight + 90;
     img.style.transform = `rotate(${rotateRight}deg)`;
   }
 
-  if (e.target.classList.contains('flip--top')) {
+  if (event === flip_left) {
     img.style.transform = `scaleX(${flipRigth})`;
     flipRigth = flipRigth === -1 ? 1 : -1;
   }
 
-  if (e.target.classList.contains('flip--bottom')) {
+  if (event === flip_btm) {
     img.style.transform = `scaleY(${flipBtm})`;
     flipBtm = flipBtm === -1 ? 1 : -1;
   }
+};
+
+sectionEditor.addEventListener('click', function (e) {
+  rotateAndFlipFunctionHandler(e.target);
 });
