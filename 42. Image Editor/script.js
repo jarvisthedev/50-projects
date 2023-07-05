ed501e07bd5d369c072368e09e8c03a90bb15b4d
dotjarvis <johnsonmakings@gmail.com>
2023-07-05 23:50:47 +0300

    305th commit
    Added half logic ... Implimenting rotation and flipping

--- 42. Image Editor/script.js
+++ 42. Image Editor/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const sectionEditor = document.querySelector('.section--editor');
+const img = document.querySelector('img');
+
+const rotate_flip = function (ph) {};
+
+sectionEditor.addEventListener('click', function (e) {
+  let rotateLeft = 0;
+  let rotateRight = 0;
+  let flipRigth = -1;
+  let flipBtm = -1;
+
+  if (e.target.classList.contains('rotate--left')) {
+    rotateLeft = rotateLeft + -90;
+    img.style.transform = `rotate(${rotateLeft}deg)`;
+  }
+
+  if (e.target.classList.contains('rotate--right')) {
+    rotateRight = rotateRight + 90;
+    img.style.transform = `rotate(${rotateRight}deg)`;
+  }
+
+  if (e.target.classList.contains('flip--top')) {
+    img.style.transform = `scaleX(${flipRigth})`;
+    flipRigth = flipRigth === -1 ? 1 : -1;
+  }
+
+  if (e.target.classList.contains('flip--bottom')) {
+    img.style.transform = `scaleY(${flipBtm})`;
+    flipBtm = flipBtm === -1 ? 1 : -1;
+  }
+});
