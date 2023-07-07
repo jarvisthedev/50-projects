d7874bc3afaaff23d131791aedd45f1804f902fc
dotjarvis <johnsonmakings@gmail.com>
2023-07-08 00:33:59 +0300

    317th commit
    Adde some logic and modification

--- 44. News app/script.js
+++ 44. News app/script.js
@@ -1,25 +1,45 @@
 `use strict`;
 
-const menuBtn = document.querySelector('.btn--menu');
-const closeBtn = document.querySelector('.btn--close');
+const btns = document.querySelector('.menu-btns');
+const btn__menu = document.querySelector('.btn--menu');
+const btn__close = document.querySelector('.btn--close');
+
+const navLinks = document.querySelector('.nav');
 const moreLinks = document.querySelector('.more');
 const searchBar = document.querySelector('.search-bar');
-const navLinks = document.querySelector('.nav');
+
+// API KEY ::: 63432cefe97c4243aa0265dec448be69
+
+const API_KEY = `63432cefe97c4243aa0265dec448be69`;
 
 let state = 'flex';
-const menuControlHelper = function () {
+let checkMoreState = 'flex';
+
+const menuControlHelper = () => {
   moreLinks.style.display = `${state}`;
   searchBar.style.display = `${state}`;
   navLinks.style.display = `${state}`;
   state = state === 'none' ? 'flex' : 'none';
   if (state === `none`) {
-    closeBtn.style.display = `block`;
-    menuBtn.style.display = `none`;
+    btn__close.style.display = `block`;
+    btn__menu.style.display = `none`;
   } else {
-    closeBtn.style.display = `none`;
-    menuBtn.style.display = `block`;
+    btn__close.style.display = `none`;
+    btn__menu.style.display = `block`;
   }
 };
 
-menuBtn.addEventListener('click', menuControlHelper);
-closeBtn.addEventListener('click', menuControlHelper);
+btns.addEventListener('click', menuControlHelper);
+navLinks.addEventListener('click', function (e) {
+  if (e.target.classList.contains('ph')) {
+    searchBar.style.display = `${checkMoreState}`;
+    moreLinks.style.display = `none`;
+  } else if (
+    !e.target.classList.contains('ph') &&
+    e.target.classList.contains('links-disappear')
+  ) {
+    moreLinks.style.display = `${checkMoreState}`;
+    searchBar.style.display = `none`;
+  }
+  checkMoreState = checkMoreState === 'none' ? 'flex' : 'none';
+});
