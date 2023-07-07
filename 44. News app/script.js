5e66a5e6fcaf08ddd91baa0108a98204b2ad348e
dotjarvis <johnsonmakings@gmail.com>
2023-07-07 20:05:19 +0300

    316th commit
    Added menu navigation and made it active

--- 44. News app/script.js
+++ 44. News app/script.js
@@ -1,1 +1,25 @@
 `use strict`;
+
+const menuBtn = document.querySelector('.btn--menu');
+const closeBtn = document.querySelector('.btn--close');
+const moreLinks = document.querySelector('.more');
+const searchBar = document.querySelector('.search-bar');
+const navLinks = document.querySelector('.nav');
+
+let state = 'flex';
+const menuControlHelper = function () {
+  moreLinks.style.display = `${state}`;
+  searchBar.style.display = `${state}`;
+  navLinks.style.display = `${state}`;
+  state = state === 'none' ? 'flex' : 'none';
+  if (state === `none`) {
+    closeBtn.style.display = `block`;
+    menuBtn.style.display = `none`;
+  } else {
+    closeBtn.style.display = `none`;
+    menuBtn.style.display = `block`;
+  }
+};
+
+menuBtn.addEventListener('click', menuControlHelper);
+closeBtn.addEventListener('click', menuControlHelper);
