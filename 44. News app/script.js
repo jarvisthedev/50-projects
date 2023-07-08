f8d54b1b3114091dcb5814ba28d89e522b01b4bf
dotjarvis <johnsonmakings@gmail.com>
2023-07-08 10:07:04 +0300

    318th commit
    Added api with some render logic

--- 44. News app/script.js
+++ 44. News app/script.js
@@ -1,5 +1,6 @@
 `use strict`;
 
+const newsContainer = document.querySelector('.section--hero .container');
 const btns = document.querySelector('.menu-btns');
 const btn__menu = document.querySelector('.btn--menu');
 const btn__close = document.querySelector('.btn--close');
@@ -8,13 +9,15 @@ const navLinks = document.querySelector('.nav');
 const moreLinks = document.querySelector('.more');
 const searchBar = document.querySelector('.search-bar');
 
-// API KEY ::: 63432cefe97c4243aa0265dec448be69
-
-const API_KEY = `63432cefe97c4243aa0265dec448be69`;
-
 let state = 'flex';
 let checkMoreState = 'flex';
 
+const API_KEY = `63432cefe97c4243aa0265dec448be69`;
+const URL = `https://newsapi.org/v2/everything?q=`;
+
+// //////////////////
+//  PROJECT UI CONTROL
+// //////////////////
 const menuControlHelper = () => {
   moreLinks.style.display = `${state}`;
   searchBar.style.display = `${state}`;
@@ -43,3 +46,57 @@ navLinks.addEventListener('click', function (e) {
   }
   checkMoreState = checkMoreState === 'none' ? 'flex' : 'none';
 });
+
+// //////////////////
+//  PROJECT API RENDERS
+// //////////////////
+const newsCardRender = function (data) {
+  const image =
+    data.articles[70]?.urlToImage &&
+    `<img src="${data.articles[70].urlToImage}" alt="Image loading" />`;
+
+  const dateObj = new Date(data.articles[70].publishedAt);
+  const dateFormat = `${dateObj.getDate().toString().padStart(2, '0')}/${(
+    dateObj.getMonth() + 1
+  )
+    .toString()
+    .padStart(2, '0')}/${dateObj.getFullYear()} ${dateObj
+    .getHours()
+    .toString()
+    .padStart(2, '0')}:${dateObj.getMinutes().toString().padStart(2, '0')}`;
+
+  let dataDescription = data.articles[70].content;
+  dataDescription = dataDescription.replace(/\s*\[\+\d+ chars\]/g, '');
+
+  const html = `
+    <div class="news-card">
+      <div
+        role="img"
+        aria-label="news-card image holder"
+        class="img-holder"
+      >
+        ${image === null ? `` : image}
+      </div>
+      <h2 class="secondary-text">
+        ${data.articles[70].title}
+      </h2>
+      <p class="date">${data.articles[70].source.name} ${dateFormat}</p>
+      <article class="blockquote">
+         ${dataDescription}
+      </article>
+    </div>
+    `;
+
+  newsContainer.innerHTML = '';
+  newsContainer.insertAdjacentHTML('afterbegin', html);
+};
+
+async function fetchNews(query) {
+  const res = await fetch(`${URL}${query}&apiKey=${API_KEY}`);
+  const data = await res.json();
+  console.log(data);
+
+  newsCardRender(data);
+}
+
+window.addEventListener('load', fetchNews('Kenya'));
