29ccbe08161df41e22454ad5e1b5eb51c0523704
dotjarvis <johnsonmakings@gmail.com>
2023-07-10 22:44:00 +0300

    330th commit
    Added some html project

---
+++ 46. Quiz app/script.js
@@ -0,0 +1,5 @@
+`use strict`;
+
+const innerContainer = document.querySelector('.section--quiz');
+
+const API_URL = `https://opentdb.com/api.php?amount=10&category=10&difficulty=medium&type=multiple`;
