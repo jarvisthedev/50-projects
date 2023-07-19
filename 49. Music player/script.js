be91c0a0a8de28a866666316a79cfb14b2d3fc05
dotjarvis <johnsonmakings@gmail.com>
2023-07-19 18:57:35 +0300

    378th commit
    Added circular progress bar line

--- 49. Music player/script.js
+++ 49. Music player/script.js
@@ -1,1 +1,11 @@
 `use strict`;
+
+// CREATING THE UI CIRCULAR SPANS
+(() => {
+  const progressBar = document.querySelector('.container--1 .img-holder');
+  for (i = 0; i < 100; i++) {
+    let span = document.createElement('span');
+    span.style.setProperty('--i', i);
+    progressBar.append(span);
+  }
+})();
