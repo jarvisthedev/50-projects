92d371677b0da34c5e6d76944acd61f041948efc
dotjarvis <johnsonmakings@gmail.com>
2023-07-24 23:35:01 +0300

    402nd commit
    All logic working as expected

--- 49. Music player/login.js
+++ 49. Music player/login.js
@@ -2,50 +2,12 @@ const section_reg = document.querySelector('.section--registation');
 const loginForm = document.querySelector('.sign-up-page');
 const signupForm = document.querySelector('.sign-in-page');
 
-section_reg.addEventListener('click', function (e) {
-  e.preventDefault();
-  const clicked = e.target;
+const name = document.querySelector('.name');
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const mail2 = document.querySelector('.mail2');
+const psrd2 = document.querySelector('.psrd2');
 
-  const name = document.querySelector('.name').value;
-  const email = document.querySelector('.email').value;
-  const password = document.querySelector('.password').value;
-
-  if (clicked.classList.contains('btn--sign-up')) {
-    if (
-      name &&
-      email &&
-      password &&
-      password.length >= 6 &&
-      email.includes('@') &&
-      email.includes('.')
-    )
-      window.location.href = 'index.html';
-    else {
-      alert('Please Enter valid email and password then try again.');
-    }
-  }
-
-  if (clicked.classList.contains('btn--sign-in')) {
-    if (
-      password &&
-      password.length >= 6 &&
-      email &&
-      email.includes('@') &&
-      email.includes('.')
-    )
-      window.location.href = 'index.html';
-    else {
-      alert('Invalid credentials. Please try again.');
-    }
-  }
-
-  if (clicked.closest('.reminder')) {
-    loginForm.classList.toggle('hidden');
-    signupForm.classList.toggle('hidden');
-  }
-});
-
-// Emaill verificaion
 const validateEmail = email => {
   return email.match(
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
@@ -53,19 +15,39 @@ const validateEmail = email => {
 };
 
 const validate = mail => {
-  if (validateEmail(mail)) {
-    console.log('valid email');
-  } else {
-    console.log('Invalid email');
-  }
-
-  // .focus()
-  return false;
+  if (validateEmail(mail)) return true;
+  else return false;
 };
 
-// Password verificaion
-function verifyPassword(password) {
+const verifyPassword = password => {
   const pattern =
     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}|:"<>?~`[\]\\;',./])(?!.*\s).{8,}$/;
   return pattern.test(password);
-}
+};
+
+name.focus();
+mail2.focus();
+
+section_reg.addEventListener('click', function (e) {
+  e.preventDefault();
+  const clicked = e.target;
+
+  const name_val = name.value;
+  const email_val = email.value;
+  const password_val = password.value;
+
+  if (clicked.classList.contains('btn--sign-up'))
+    if (name_val && validate(email_val) && verifyPassword(password_val))
+      window.location.href = 'index.html';
+    else alert('Please Enter valid email and password then try again.');
+
+  if (clicked.classList.contains('btn--sign-in'))
+    if (verifyPassword(psrd2.value) && validate(mail2.value))
+      window.location.href = 'index.html';
+    else alert('Invalid credentials. Please try again.');
+
+  if (clicked.closest('.reminder')) {
+    loginForm.classList.toggle('hidden');
+    signupForm.classList.toggle('hidden');
+  }
+});
