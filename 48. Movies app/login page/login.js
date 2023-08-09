2d976c0b9035a7cea99a71cc0cf0c47f8a228651
dotjarvis <johnsonmakings@gmail.com>
2023-08-09 21:21:07 +0300

    480th commit
    Modified and added ALERT box

--- 48. Movies app/login page/login.js
+++ 48. Movies app/login page/login.js
@@ -1,6 +1,8 @@
 `use script`;
 
 const sectionSign = document.querySelector('.section--signInForm');
+const signup_login = document.querySelectorAll('.sign p');
+const btn_signup_login = document.querySelectorAll('.auth');
 
 const username = document.querySelector('.username');
 const email = document.querySelector('.email');
@@ -10,7 +12,8 @@ const checkPassword = password => {
   const paswd = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])/;
   if (paswd.test(password.value)) return true;
   else {
-    alert('Use correct password format');
+    // alert();
+    showAlert('Use correct password format', 3000);
     return false;
   }
 };
@@ -20,12 +23,12 @@ const validateEmail = email => {
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   if (email.value.match(mail)) return true;
-  else alert('Must be a valid email');
+  else showAlert('Must be a valid email', 3000);
 };
 
 const showAlert = (message, duration) => {
   const alertBox = document.createElement('div');
-  alertBox.className = 'custom-alert';
+  alertBox.className = 'alert-box';
   alertBox.textContent = message;
   document.body.appendChild(alertBox);
   setTimeout(() => {
@@ -38,12 +41,24 @@ sectionSign.addEventListener('click', e => {
   e.preventDefault();
   const clicked = e.target;
 
-  if (clicked.closest('.sigh-up')) {
-    if (username && validateEmail(email) && checkPassword(password)) {
-      window.location.href = `../index.html`;
-      showAlert('Login success', 3000);
-    }
+  if (clicked.closest('.auth')) {
+    if (!email.hidden)
+      if (username && validateEmail(email) && checkPassword(password)) {
+        window.location.href = `../index.html`;
+        showAlert('Login success', 3000);
+      }
+
+    if (email.hidden)
+      if (username && checkPassword(password)) {
+        window.location.href = `../index.html`;
+        showAlert('Login success', 3000);
+      }
+
+    usernamepassword.value = '';
+    emailpassword.value = '';
+    password.value = '';
   }
+
   if (clicked.closest('.google'))
     window.location.href = ' https://accounts.google.com/o/oauth2/auth/';
 
@@ -55,4 +70,10 @@ sectionSign.addEventListener('click', e => {
 
   if (clicked.closest('.apple'))
     window.location.href = `https://www.icloud.com/`;
+
+  if (clicked.closest('.sign')) {
+    signup_login.forEach(p => p.classList.toggle('hidden'));
+    btn_signup_login.forEach(p => p.classList.toggle('hidden'));
+    email.hidden = !email.hidden;
+  }
 });
