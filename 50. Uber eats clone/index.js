47854d39602962021cf78e091aa04fe0f12371db
dotjarvis <johnsonmakings@gmail.com>
2023-08-15 10:37:30 +0300

    502nd commit
    Added header and aside section styling

--- 50. Uber eats clone/index.js
+++ 50. Uber eats clone/index.js
@@ -1,13 +1,16 @@
 const menu_bar = document.querySelector(".bx-menu");
-const overlay = document.querySelector(".blur");
-const hidden_menu = document.querySelector(".mini-menu");
+const empty_div = document.querySelector(".empty");
+const section_aside = document.querySelector(".section--aside");
 
-menu_bar.addEventListener("click", function () {
-  hidden_menu.classList.add("visible");
-  overlay.classList.add("overlay");
+menu_bar.addEventListener("click", () => {
+  section_aside.classList.add("visible");
+  empty_div.classList.add("blur");
 });
 
-overlay.addEventListener("click", function () {
-  overlay.classList.remove("overlay");
-  hidden_menu.classList.remove("visible");
+section_aside.addEventListener("click", function (e) {
+  const clicked = e.target;
+  if (clicked.closest(".blank")) {
+    empty_div.classList.remove("blur");
+    section_aside.classList.remove("visible");
+  }
 });
